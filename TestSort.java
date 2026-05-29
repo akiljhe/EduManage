fad0241046b713b46b667180caebefea91d0c138
akiljhe <aqiljhe@gmail>
2026-05-30 01:41:44 +0800

    chore: bersih-bersih file eksperimen yang tidak terpakai

--- TestSort.java
+++
@@ -1,7 +0,0 @@
-import javafx.scene.control.TableView;
-public class TestSort {
-    public static void main(String[] args) {
-        TableView<String> tv = new TableView<>();
-        tv.sortPolicyProperty().set(t -> true);
-    }
-}
